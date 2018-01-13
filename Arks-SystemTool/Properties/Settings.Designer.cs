ca68590eb949e8f73bc69d9dc184670729b3f69f
SenpaiSilver <silvermesprims@gmail.com>
2018-01-13 16:12:36 +0100

    Correcte a bug during the first launch with the version numbers

--- Arks-SystemTool/Properties/Settings.Designer.cs
+++ Arks-SystemTool/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Arks_SystemTool.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -109,7 +109,7 @@ namespace Arks_SystemTool.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v50001_rc_28")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.0.0.0")]
         public string current_patch_version {
             get {
                 return ((string)(this["current_patch_version"]));
